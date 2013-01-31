76d64ea99fa0e56d9587b814348098e95775d6b0
Fran Moreno <franmomu@gmail.com>
2013-01-31 01:56:06 +0100

    Added default configuration

--- src/FranMoreno/Silex/Provider/PagerfantaServiceProvider.php
+++ src/FranMoreno/Silex/Provider/PagerfantaServiceProvider.php
@@ -15,6 +15,15 @@ class PagerfantaServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
+        $app['pagerfanta.view.default_options'] = array(
+            'routeName'     => null,
+            'routeParams'   => array(),
+            'pageParameter' => '[page]',
+            'proximity' => 3,
+            'next_message' => '&raquo;',
+            'prev_message' => '&laquo;'
+        );
+
         $app['pagerfanta.view_factory'] = $app->share(function ($app) {
             $defaultView = new DefaultView();
             $twitterBoostrapView = new TwitterBootstrapView();
@@ -31,6 +40,9 @@ class PagerfantaServiceProvider implements ServiceProviderInterface
 
     public function boot(Application $app)
     {
+        $options = isset($app['pagerfanta.view.options']) ? $app['pagerfanta.view.options'] : array();
+        $app['pagerfanta.view.options'] = array_replace($app['pagerfanta.view.default_options'], $options);
+
         if (isset($app['twig'])) {
             $app['twig']->addExtension(new PagerfantaExtension($app));
         }
