499bd054d63f856c25c46d2e98e7d6d63d79abe1
Fran Moreno <franmomu@gmail.com>
2013-03-24 17:20:00 +0100

    Added getForDoctrineORM to PagerfantaFactory

--- src/FranMoreno/Silex/Service/PagerfantaFactory.php
+++ src/FranMoreno/Silex/Service/PagerfantaFactory.php
@@ -3,7 +3,9 @@
 namespace FranMoreno\Silex\Service;
 
 use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\AdapterInterface;
 use Pagerfanta\Adapter\ArrayAdapter;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
 
 class PagerfantaFactory
 {
@@ -11,6 +13,18 @@ class PagerfantaFactory
     {
         $adapter = new ArrayAdapter($array);
 
+        return $this->createPagerfanta($adapter);
+    }
+
+    public function getForDoctrineORM($query, $fetchJoinCollection = true)
+    {
+        $adapter = new DoctrineORMAdapter($query, $fetchJoinCollection);
+
+        return $this->createPagerfanta($adapter);
+    }
+
+    protected function createPagerfanta(AdapterInterface $adapter)
+    {
         return new Pagerfanta($adapter);
     }
 }
